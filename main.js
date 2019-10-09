e25773db2fb418902a5afb01a59ac43791837c38
Dávid Mátyás <davimatyi@outlook.com>
2019-10-09 12:27:46 +0200

    fixed missing window controls

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 250,
     height: 250,
-    frame: false,
+    frame: true,
     resizable: false,
     maximizable: false,
     titleBarStyle: "hiddenInset",
@@ -19,6 +19,7 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+  mainWindow.removeMenu()
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
