9001f9a0de05f5e05a3c84f02ab0fe0fac340c3d
Marcell Schuh <marcell.schuh@gmail.com>
2020-10-01 20:24:11 +0200

    Fancy title bar

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -10,8 +10,8 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 250,
-    height: 250,
-    frame: true,
+    height: 280,
+    frame: false,
     resizable: false,
     maximizable: false,
     titleBarStyle: "hiddenInset",
@@ -20,6 +20,8 @@ function createWindow () {
     }
   })
   mainWindow.removeMenu()
+
+  mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -52,7 +54,15 @@ app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
+
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on('close-window', () => {
+  mainWindow.close()
+});
+
+ipcMain.on('minimize-window', () => {
+  mainWindow.minimize()
+})
